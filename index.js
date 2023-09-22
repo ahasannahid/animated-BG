7d802365c88206aa3485894e1fd3895b8035fe56
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-09-23 00:39:37 +0600

    task finished

---
+++ index.js
@@ -0,0 +1,14 @@
+function createcubes() {
+    const numcubes = 150; 
+
+    for (let i = 0; i < numcubes; i++) {
+        const cube = document.createElement("div");
+        cube.className = "cube";
+        cube.style.top = Math.random() * 100 + "%";
+        cube.style.left = Math.random() * 100 + "%";
+        cube.style.animationDelay = Math.random() + "s";
+        document.body.appendChild(cube);
+    }
+}
+
+createcubes();
